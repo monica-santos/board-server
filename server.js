21e5ecab28849503e639e1b8607e3861f3e8824d
Mônica <monica.santos@itlab.com.br>
2019-03-17 15:09:59 -0300

    estrutura das pastas

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { ApolloServer } = require('apollo-server')
-const typeDefs = require('./typeDefs')
-const resolvers = require('./resolvers')
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers')
 
 const server = new ApolloServer({
   typeDefs,
